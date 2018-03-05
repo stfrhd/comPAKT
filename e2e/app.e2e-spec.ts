671ec2bddfebbc73bbcab0db3de3cdd6b9e669e8
Stefan Rohde <rohdes@int.root.msg.ag>
2018-03-05 12:57:15 +0100

    update

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { ComPAKTPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('com-pakt App', function() {
-  let page: ComPAKTPage;
+describe('com-pakt App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new ComPAKTPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
