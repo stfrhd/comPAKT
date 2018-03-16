b5b9eb74044b931f7efd13ac327611d592c007aa
Stefan Rohde <rohdes@int.root.msg.ag>
2018-03-16 13:20:50 +0100

    erste Struktur

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SearchComponent } from './search/search.component';
+import { TodoComponent } from './todo/todo.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FavComponent } from './fav/fav.component';
 
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'todo', component: TodoComponent },
+  { path: 'fav', component: FavComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    SearchComponent,
+    TodoComponent,
+    PageNotFoundComponent,
+    FavComponent
   ],
   imports: [
+    RouterModule.forRoot( appRoutes, { enableTracing: true } ),
     BrowserModule
   ],
   providers: [],
