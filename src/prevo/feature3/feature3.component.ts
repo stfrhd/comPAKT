b32835b7128df5a36bab9c3910b0674a9aabec8b
Stefan Rohde <stefan.rohde@prevo.ch>
2018-04-12 10:29:01 +0200

    update

--- src/prevo/feature3/feature3.component.ts
+++ src/prevo/feature3/feature3.component.ts
@@ -38,14 +38,14 @@ export interface Element {
 }
 
 const ELEMENT_DATA: Element[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 1, name: 'Dienstaustritt', weight: 1.0079, symbol: 'Hans Mueller'},
+  {position: 2, name: 'Gehaltsänderung', weight: 4.0026, symbol: 'Stefan Rohde'},
+  {position: 3, name: 'Heirat', weight: 6.941, symbol: 'Boris Becker'},
+  {position: 4, name: 'Personendaten', weight: 9.0122, symbol: 'Police 12345'},
+  {position: 5, name: 'Anfrage', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Offerte', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Neueintritt', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Meldung', weight: 15.9994, symbol: 'O'},
   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
   {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
